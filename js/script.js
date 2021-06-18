10e3df48d8ba3cfb5eb53111ff764c88571c80f1
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-06-19 05:31:32 +0600

    three js applied

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('.my-div'),
+    intensity: 0.3,
+    image1: 'https://i.ibb.co/237gNDT/formschon.png',
+    image2: 'https://i.ibb.co/bdWYntp/neuman.png',
+    displacementImage: '../img/heightMap.png'
+});
